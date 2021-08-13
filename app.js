aeda7d20fcfa0547657400771691a3ed05250a7f
Michael Rider <mikepdxrider@gmail.com>
2021-08-13 13:47:44 -0700

    Implemented state management and event handlers.

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // import functions and grab DOM elements
 
 // initialize state
+let hawksCounter = 0;
+let ospreysCounter = 0;
+// set event listeners
+const hawksScoreEl = document.querySelector('#hawks-score');
+const ospreysScoreEl = document.querySelector('#ospreys-score');
+const hawksButtonEl = document.querySelector('.hawks-button');
+const ospreysButtonEl = document.querySelector('.ospreys-button');
+// console.log( hawksScoreEl, ospreyScoreEl, hawksButtonEl, ospreysButtonEl);
+// STRETCH: Anouncer selector
 
-// set event listeners 
   // get user input
+hawksButtonEl.addEventListener('click', () => {
+    hawksCounter += 7;
+    // console.log(hawksCounter);
+    hawksScoreEl.textContent = hawksCounter;
+});
+
+ospreysButtonEl.addEventListener('click', () => {
+    ospreysCounter += 7;
+    // console.log(ospreysCounter);
+    ospreysScoreEl.textContent = ospreysCounter;
+});
   // use user input to update state 
   // update DOM to reflect the new state
